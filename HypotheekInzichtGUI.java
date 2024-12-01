1ee5893bcf5bd9b927c3582167644415f9d36737
Gijs Cleophas <146765318+GCleophas@users.noreply.github.com>
2024-12-01 17:01:21 +0100

    Update HypotheekInzichtGUI.java

--- HypotheekInzichtGUI.java
+++ HypotheekInzichtGUI.java
@@ -1,74 +1,112 @@
 package hypotheekinzicht;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Image;
-import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author GCleophas
  * logo kleurcode: 107,8,82
  */
-public class HypotheekInzichtGUI {
+public class HypotheekInzichtGUI implements ActionListener {
+    private static JFrame frame;
+    private static JPanel panel;
+    private static JButton home;
+    private static JLabel textBeschrijving;
+    private static JTextField inputBeschrijving;
+    private static JLabel textSom;
+    private static JTextField inputSom;
+    private static JLabel textRente;
+    private static JTextField inputRente;
+    private static JLabel textLooptijd;
+    private static JTextField inputLooptijd;
+    private static JButton bereken;
     
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Image import
-        ImageIcon image1 = new ImageIcon("/Users/gijscleophas/Software Development/HypotheekInzicht/Afbeeldingen/huis.png");
-        ImageIcon image2 = new ImageIcon("/Users/gijscleophas/Software Development/HypotheekInzicht/Afbeeldingen/document.png");
-        Image scaledHome = image1.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        ImageIcon imageHome = new ImageIcon(scaledHome);
-        Image scaledResult = image2.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        ImageIcon imageResult = new ImageIcon(scaledResult);
-        
-        //Frame settings
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
+        //Frame
+        frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setSize(1280,720);
+        frame.setSize(500,250);
         frame.setTitle("Hypotheek inzicht");
         
-        //Panel menu
-        JPanel panelMenu = new JPanel();
-        panelMenu.setBackground(Color.WHITE);
-        panelMenu.setPreferredSize(new Dimension(100,75));
-        panelMenu.setLayout(new FlowLayout(FlowLayout.LEFT));
-        frame.add(panelMenu,BorderLayout.NORTH);
+        //Panel
+        panel = new JPanel();
+        panel.setLayout(null);
+        frame.add(panel);
         
-        //Button home
-        JButton buttonHome = new JButton();
-        buttonHome.setIcon(imageHome);
-        panelMenu.add(buttonHome);
+        //Home button
+        home = new JButton("Home");
+        home.setBounds(10, 10, 100, 25);
+        //home.addActionListener(new HypotheekInzichtGUI());
+        panel.add(home);
         
-        //Panel info
-        JPanel panelInfo = new JPanel();
-        panelInfo.setBackground(Color.DARK_GRAY);
-        panelInfo.setPreferredSize(new Dimension(300,75));
-        frame.add(panelInfo,BorderLayout.WEST);
+        //Hypotheek type (JComboBox)
         
-        //Button result
-        JButton buttonResult = new JButton();
-        buttonResult.setIcon(imageResult);
-        buttonResult.setText("Bereken hypotheek");
-        buttonResult.setForeground(new Color(107,8,82));
-        buttonResult.setHorizontalTextPosition(JButton.RIGHT);
-        panelInfo.add(buttonResult, BorderLayout.SOUTH);
+        //Hypotheek beschrijving
+        textBeschrijving = new JLabel("Hypotheek beschrijving:");
+        textBeschrijving.setBounds(50, 50, 200, 25);
+        panel.add(textBeschrijving);
+        inputBeschrijving = new JTextField();
+        inputBeschrijving.setBounds(250, 50, 200, 25);
+        panel.add(inputBeschrijving);
         
-        //Panel result
-        JPanel panelResult = new JPanel();
-        panelResult.setBackground(Color.LIGHT_GRAY);
-        panelResult.setPreferredSize(new Dimension(980,75));
-        frame.add(panelResult,BorderLayout.EAST);
+        //Hypotheek som
+        textSom = new JLabel("Hypotheek som:");
+        textSom.setBounds(50, 75, 200, 25);
+        panel.add(textSom);
+        inputSom = new JTextField();
+        inputSom.setBounds(250, 75, 200, 25);
+        panel.add(inputSom);
+        
+        //Hypotheek rente
+        textRente = new JLabel("Hypotheek rente:");
+        textRente.setBounds(50, 100, 200, 25);
+        panel.add(textRente);
+        inputRente = new JTextField();
+        inputRente.setBounds(250, 100, 200, 25);
+        panel.add(inputRente);
+        
+        //Hypotheek looptijd
+        textLooptijd = new JLabel("Hypotheek looptijd:");
+        textLooptijd.setBounds(50, 125, 200, 25);
+        panel.add(textLooptijd);
+        inputLooptijd = new JTextField();
+        inputLooptijd.setBounds(250, 125, 200, 25);
+        panel.add(inputLooptijd);
+        
+        //Bereken button
+        bereken = new JButton("Bereken hypotheek");
+        bereken.setBounds(150, 175, 200, 25);
+        bereken.addActionListener(new HypotheekInzichtGUI());
+        panel.add(bereken);
+        
+        frame.setVisible(true);
     }
+
+    //Momenteel nog maar type hypotheek
+    @Override //Bereken button
+    public void actionPerformed(ActionEvent h) {
+        try {
+            String beschrijving = inputBeschrijving.getText();
+            double rente = Double.parseDouble(inputRente.getText());
+            double som = Double.parseDouble(inputSom.getText());
+            int looptijd = Integer.parseInt(inputLooptijd.getText());
+            
+            LineaireHypotheek hypotheek1 = new LineaireHypotheek(001, beschrijving, rente, som, looptijd);
+            hypotheek1.getHypotheekInzicht();
+        } catch (NegativeValueException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
     
 }
