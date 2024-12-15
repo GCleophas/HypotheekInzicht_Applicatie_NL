5a20136171fb14db4c2be39b21a8857b46057247
Gijs Cleophas <146765318+GCleophas@users.noreply.github.com>
2024-12-15 21:58:56 +0100

    Update SpaarHypotheek.java

--- SpaarHypotheek.java
+++ SpaarHypotheek.java
@@ -9,8 +9,8 @@ public class SpaarHypotheek extends Hypotheek {
     private double gespaardBedrag;
     
     //Constructors
-    public SpaarHypotheek(int hypotheekNummer, String hypotheekBeschrijving, double hypotheekRente, double hypotheekSom, int hypotheekLooptijd) throws NegativeValueException{
-        super(hypotheekNummer, hypotheekBeschrijving, hypotheekRente, hypotheekSom, hypotheekLooptijd);
+    public SpaarHypotheek(String hypotheekBeschrijving, double hypotheekRente, double hypotheekSom, int hypotheekLooptijd) throws NegativeValueException{
+        super(hypotheekBeschrijving, hypotheekRente, hypotheekSom, hypotheekLooptijd);
         this.inleg = berekenInleg();
         this.gespaardBedrag = 0;
     }
@@ -30,8 +30,9 @@ public class SpaarHypotheek extends Hypotheek {
         return getHypotheekSom() / som;
     }
     
-    @Override
+    @Override //HypotheekInzicht
     public void getHypotheekInzicht() {
+        System.out.println(getHypotheekNummer() + " " + getHypotheekBeschrijving());
         System.out.printf("Spaarhypotheek: Hoofdsom = %.2f, Rente = %.2f%%, Looptijd = %d jaar%n", getHypotheekSom(), getHypotheekRente(), getHypotheekLooptijd());
         System.out.println("Jaar\tInleg\tGespaard\tRente");
         
@@ -42,5 +43,44 @@ public class SpaarHypotheek extends Hypotheek {
             gespaardBedrag += inleg + renteGespaardBedrag;
             System.out.printf("%d\t%.2f\t%.2f\t%.2f%n", jaar, inleg, gespaardBedrag, rente);
         }
+    }
+    
+    @Override //LauncherV1
+    public String getHypotheekInzichtGUI() {
+        StringBuilder result = new StringBuilder();
+        
+        result.append("<html>");
+        result.append(String.format("<p>Jaar&nbsp;&nbsp;Inleg&nbsp;&nbsp;&nbsp;Gespaard&nbsp;&nbsp;&nbsp;Rente</p>"));
+        
+        double rente = getHypotheekSom() * (getHypotheekRente() / 100);
+        
+        for(int jaar = 1; jaar <= getHypotheekLooptijd(); jaar++) {
+            double renteGespaardBedrag = gespaardBedrag * (getHypotheekRente() / 100);
+            gespaardBedrag += inleg + renteGespaardBedrag;
+            result.append(String.format("<p>%d&nbsp;&nbsp;%.2f&nbsp;&nbsp;&nbsp;%.2f&nbsp;&nbsp;&nbsp;%.2f</p>", jaar, inleg, gespaardBedrag, rente));
+        }
+        result.append("<html>");
+        return result.toString();
+    }
+    
+    @Override //LauncherV2
+    public void berekenHypotheek() {
+        
+        double rente = getHypotheekSom() * (getHypotheekRente() / 100);
+        
+        for (int jaar = 1; jaar <= getHypotheekLooptijd(); jaar++) {
+            double renteGespaardBedrag = gespaardBedrag * (getHypotheekRente() / 100);
+            gespaardBedrag += inleg + renteGespaardBedrag;
+            
+            resultJaar.append(jaar).append("<br>");
+            resultInleg.append((String.format("%.2f", (double) inleg))).append("<br>");
+            resultGespaardBedrag.append((String.format("%.2f", (double) gespaardBedrag))).append("<br>");
+            resultRente.append((String.format("%.2f", (double) rente))).append("<br>");
+        }
+        
+        resultJaar.append("</div></html>");
+        resultInleg.append("</div></html>");
+        resultGespaardBedrag.append("</div></html>");
+        resultRente.append("</div></html>");
     }
 }
